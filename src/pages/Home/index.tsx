3e36cccec1a03ab9872f2231e1debf281114b939
Svitlana-G <svitlana.golubieva@gmail.com>
2026-03-13 14:54:01 +0100

    Add button to home page for testing

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,17 @@
+import Button from '@components/Button/Button';
+
 export const HomePage = () => {
-  return <h1>Home page</h1>;
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: '50px'
+      }}
+    >
+      <h1>Home page</h1>
+      <Button label="Join to us" />
+    </div>
+  );
 };
