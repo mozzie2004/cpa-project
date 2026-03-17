01252d46edfc2a2fd3ade84f6dd95662db91bc91
Svitlana-G <svitlana.golubieva@gmail.com>
2026-03-17 14:40:01 +0100

    Remove button from Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,3 @@
-import Button from '@components/Button/Button';
-
 export const HomePage = () => {
-  return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        gap: '50px'
-      }}
-    >
-      <h1>Home page</h1>
-      <Button label="Join to us" />
-    </div>
-  );
+  return <h1>Home page</h1>;
 };
